b8ce89d5080931c08ca8279770256603aabfa9be
ShubhamSoni <shubham.soni@metacube.com>
2021-04-08 10:49:44 +0530

    updation in A9

---
+++ A6/Assets/js/app.ts
@@ -0,0 +1,9 @@
+var employee = new EmployeeDetails();
+var vehicle =  new Vehicle();
+var pass = new Pass();
+
+
+// //Anonymous function which initialze all the price tag nodes for further updation
+// (function(){
+//     payment.getDomObjects();
+// })();
